84f19e11f40e8aae5ca2089f6b0f0a92e9d1ebe6
David Endo <davidhayaku@gmail.com>
2022-09-12 22:31:22 -0300

    Inicio do projeto

---
+++ src/test/java/curso/rest/api/CursospringrestapiApplicationTests.java
@@ -0,0 +1,13 @@
+package curso.rest.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursospringrestapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
